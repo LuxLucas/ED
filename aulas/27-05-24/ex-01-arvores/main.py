9b3968d4c4ce77382fe520c2804036983280225a
Lucas Flôres <124315388+LuxLucas@users.noreply.github.com>
2024-05-27 21:52:05 -0300

    Add files via upload

---
+++ aulas/27-05-24/ex-01-arvores/main.py
@@ -0,0 +1,73 @@
+from NodoArvore import NodoArvore
+
+
+def tem_esquerda(no):
+    return no.esquerda is not None
+
+
+def tem_direita(no):
+    return no.direita is not None
+
+
+def tem_filhos(no):
+    return tem_esquerda(no) or tem_direita(no)
+
+
+def valor_e_igual(valor, no):
+    return valor == no.chave
+
+
+def valor_e_maior(valor, no):
+    return valor > no.chave
+
+
+def valor_e_menor(valor, no):
+    return valor < no.chave
+
+
+def obter_no_direito_se_existir(no):
+    if tem_direita(no):
+        no = no.obter_direita()
+        return no
+
+
+def obter_no_esquerdo_se_existir(no):
+    if tem_esquerda(no):
+        no = no.obter_esquerda()
+        return no
+
+
+def obter_no_mais_proximo_da_pesquisa(pesquisa, no):
+    if valor_e_maior(pesquisa, no):
+        no = obter_no_direito_se_existir(no)
+    if valor_e_menor(pesquisa, no):
+        no = obter_no_esquerdo_se_existir(no)
+    return no
+
+
+def existe_na_arvore(pesquisa, raiz):
+    resultado_encontrado = False
+    no = raiz
+    while not (resultado_encontrado or resultado_encontrado is None):
+        if valor_e_igual(pesquisa, no):
+            return True
+        if tem_filhos(no):
+            no = obter_no_mais_proximo_da_pesquisa(pesquisa, no)
+        else:
+            return False
+
+
+def main():
+    raiz = NodoArvore(43)
+
+    raiz.esquerda = NodoArvore(11)
+    raiz.direita = NodoArvore(62)
+
+    raiz.direita.esquerda = NodoArvore(48)
+    raiz.direita.direita = NodoArvore(95)
+    raiz.esquerda.esquerda = NodoArvore(9)
+    raiz.direita.direita = NodoArvore(41)
+
+
+if __name__ == '__main__':
+    main()
